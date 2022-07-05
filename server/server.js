877f0a516062f254755e1b5e3c4e35ccb6e8aa76
Dave Sanders <davepaulsanders@gmail.com>
2022-07-05 15:40:24 -0400

    Set up concurrently and client folder

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const { authMiddleware } = require("./utils/auth");
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
-
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
@@ -17,6 +17,14 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
