46f1e2fd6b9b1131f65689c8f6e9566183925e75
Dave Sanders <davepaulsanders@gmail.com>
2022-07-07 16:22:19 -0400

    Fixing auth import

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { ADD_THOUGHT } from "../../utils/mutations";
 import { QUERY_THOUGHTS, QUERY_ME } from "../../utils/queries";
+
 const ThoughtForm = () => {
   const [thoughtText, setText] = useState("");
   const [characterCount, setCharacterCount] = useState(0);
--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -4,7 +4,7 @@ import { useQuery } from "@apollo/client";
 import { QUERY_THOUGHT } from "../utils/queries";
 import ReactionList from "../components/ReactionList";
 import ReactionForm from "../components/ReactionForm";
-import { Auth } from "../utils/auth";
+import Auth from "../../utils/auth";
 
 const SingleThought = (props) => {
   const { id: thoughtId } = useParams();
